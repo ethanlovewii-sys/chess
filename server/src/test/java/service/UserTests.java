ff042a1be4f7e3f15a14b2d3717fda541d8ee45b
Ethan Peterson <eap731@byu,edu>
2026-07-15 15:39:06 -0600

    Added Login tests

--- server/src/test/java/service/UserTests.java
+++ server/src/test/java/service/UserTests.java
@@ -1,13 +1,16 @@
 package service;
 
-import dataaccess.AuthDAO;
-import dataaccess.MemoryAuthDAO;
-import dataaccess.MemoryUserDAO;
-import dataaccess.UserDAO;
+import dataaccess.*;
 import model.UserData;
+import org.eclipse.jetty.util.log.Log;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import passoff.model.TestAuthResult;
 import passoff.model.TestUser;
+import request.LoginRequest;
 import request.RegisterRequest;
+import result.LoginResult;
 import result.RegisterResult;
 import server.Exception.AlreadyTakenException;
 import server.ResponseException;
@@ -18,19 +21,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserTests {
+
     UserDAO userDAO = new MemoryUserDAO();
     AuthDAO authDAO = new MemoryAuthDAO();
 
     UserService service = new UserService(userDAO, authDAO);
+
+    @BeforeEach
+    public void setup() throws ResponseException, DataAccessException {
+        service.clear();
+        //one user already logged in
+        service.register(new RegisterRequest("ExistingUser", "existingUserPassword", "eu@mail.com"));
+    }
 
     @Test
     void register() throws Exception{
         RegisterRequest newUser = new RegisterRequest("NewUser", "NewUserPassword", "nu@mail.com");
         service.register(newUser);
         UserData userFound = userDAO.getUser("NewUser");
-        assertEquals(userFound.username(), "NewUser");
-        assertEquals(userFound.password(), "NewUserPassword");
-        assertEquals(userFound.email(), "nu@mail.com");
+        assertEquals("NewUser", userFound.username());
+        assertEquals("NewUserPassword", userFound.password());
+        assertEquals("nu@mail.com", userFound.email());
     }
 
     @Test
@@ -40,6 +51,20 @@ public class UserTests {
 
         assertThrows(ResponseException.class, () -> {
             service.register(newUser);
+        });
+    }
+
+    @Test
+    void normalLogin() throws Exception{
+        LoginResult loggedInUser = service.login(new LoginRequest("ExistingUser","existingUserPassword"));
+        assertEquals("ExistingUser", loggedInUser.username());
+        Assertions.assertNotNull(loggedInUser.authToken());
+    }
+
+    @Test
+    void badPasswordLogin() {
+        assertThrows(ResponseException.class, () -> {
+            service.login(new LoginRequest("ExistingUser","BADPASSWORD"));
         });
     }
 }
